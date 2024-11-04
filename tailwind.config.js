ef1509977f05ea8ab88c09c7c8a8237de0c68c46
Muhammad Madhani Putra <muhammad.madhani@ui.ac.id>
2024-11-05 00:09:43 +0700

    feat: Finish Configuration and Make Body to Phone Size

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx,vue}', './index.html'],
+  theme: {
+    extend: {
+      boxShadow: {
+        custom: '-5px 0 80px -5px rgba(0, 0, 0, 0.3)',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
