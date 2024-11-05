f70771cc58faa3efae687032e30305aded5b9a58
Muhammad Madhani Putra <muhammad.madhani@ui.ac.id>
2024-11-05 13:04:58 +0700

    feat: Finish Intro Page

---
+++ src/components/elements/Indicator.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+import ButtonIndicator from '@/assets/images/ButtonIndicator.vue';
+
+const props = defineProps<{
+  activeIndex: number;
+}>();
+
+// const emits = defineEmits<{
+//   (e: 'update:activeIndex', index: number): void;
+// }>();
+
+</script>
+
+<template>
+  <div class="flex flex-row gap-2">
+    <ButtonIndicator :isActive="activeIndex" />
+  </div>
+</template>
