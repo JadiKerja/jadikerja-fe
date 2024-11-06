57cd0aabb1d99030f7ee0ec9c384072dfe7f83e3
Muhammad Madhani Putra <muhammad.madhani@ui.ac.id>
2024-11-06 13:44:12 +0700

    feat: Profile landing page and Logout

---
+++ src/modules/ProfileModule/Profile.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import TransparentCircle from '@/components/elements/TransparentCircle.vue';
+import IconProfile from '@/assets/images/IconProfile.vue';
+import { computed } from 'vue';
+import { useAuthStore } from '@/stores/userStores';
+import { RouterLink } from 'vue-router';
+import EditProfileIcon from '@/assets/images/EditProfileIcon.vue';
+import BuildingIcon from '@/assets/images/BuildingIcon.vue';
+import InfoIcon from '@/assets/images/InfoIcon.vue';
+import LamaranIcon from '@/assets/images/LamaranIcon.vue';
+import LogoutIcon from '@/assets/images/LogoutIcon.vue';
+import Cookies from 'js-cookie'
+
+const profileStore = useAuthStore();
+const profileImageUrl = computed(() => profileStore.user.client.profileUrl);
+
+function handleLogout() {
+  profileStore.setUser(null)
+  Cookies.remove('accessToken')
+}
+
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col items-center w-full">
+    <div class="bg-[rgb(214,39,39)] flex flex-col items-center relative w-full pt-6 pb-[2.5rem] gap-6 overflow-hidden">
+      <TransparentCircle class="absolute -top-[4rem] -right-[6rem] bg-[#E55A24B2] w-[10.66975rem] h-[10.66975rem]" />
+      <TransparentCircle class="absolute -top-[3rem] -right-[5rem] bg-[#E55A2466] w-[10.66975rem] h-[10.66975rem]" />
+      <TransparentCircle class="absolute top-[10rem] -left-[7rem] bg-[#E55A24B2] w-[10.66975rem] h-[10.66975rem]" />
+      <TransparentCircle class="absolute top-[12rem] -left-[6rem] bg-[#E55A2466] w-[10.66975rem] h-[10.66975rem]" />
+      
+      <p class="text-[1.75rem] font-bold text-white tracking-[0.02188rem]">Profil Akun</p>
+
+      <div class="flex relative items-center justify-center w-[5.9375rem] h-[6.1875rem] mt-4">
+        <div
+          class="absolute w-full h-full rounded-full"
+          :style="{
+            backgroundImage: profileImageUrl ? `url(${profileImageUrl})` : '',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }"
+        >
+          <IconProfile v-if="!profileImageUrl" class="w-full h-full" />
+        </div>
+      </div>
+      <p class="text-[1.25rem] font-semibold tracking-[.01563rem] text-white">
+        {{ profileStore.user.client.fullName }}
+      </p>
+    </div>
+    <div class="flex flex-col px-7 pt-5 pb-[7rem] text-black font-semibold w-full gap-5">
+      <p class="text-[1.25rem] tracking-[.01563rem]">Pengaturan Akun</p>
+      <div class="flex flex-col gap-4">
+        <RouterLink
+        to="/profile/edit"
+        class="flex flex-row gap-8 items-center">
+          <EditProfileIcon />
+          Edit Profil
+        </RouterLink>
+        <RouterLink
+        to="/profile/lamaran"
+        class="flex flex-row gap-8 items-center">
+          <LamaranIcon />
+          Lamaran Saya
+        </RouterLink>
+        <RouterLink
+        to="/profile/pekerjaan"
+        class="flex flex-row gap-8 items-center">
+          <BuildingIcon />
+          Daftar Pekerjaan
+        </RouterLink>
+        <RouterLink
+        to="/profile/snk"
+        class="flex flex-row gap-8 items-center">
+          <InfoIcon/>
+          Syarat dan Ketentuan
+        </RouterLink>
+        <RouterLink
+        to="/login"
+        class="flex flex-row gap-8 items-center"
+        @click="handleLogout">
+          <LogoutIcon />
+          Keluar
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
--- src/stores/userStores.ts
+++ src/stores/userStores.ts
@@ -3,17 +3,41 @@ import { ref, computed, watchEffect } from 'vue'
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
+enum Role {
+  Client = 'CLIENT',
+  Admin = 'ADMIN',
+  Guest = 'GUEST',
+}
+
+interface Client {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  fullName: string;
+  birthDate: string;
+  domicile: string;
+  phone: string;
+  profileUrl: string;
+  userEmail: string;
+}
+
+interface User {
+  email: string;
+  role: Role;
+  client: Client | null;
+}
+
 export const useAuthStore = defineStore('auth', () => {
-  const user = ref(null)
-  const accessToken = ref(Cookies.get('accessToken') || '')
-  const isLoading = ref(false)
-  const isLoggedIn = computed(() => !!user.value)
+  const user = ref<User | null>(null);
+  const accessToken = ref(Cookies.get('accessToken') || '');
+  const isLoading = ref(false);
+  const isLoggedIn = computed(() => !!user.value);
 
   async function fetchUserData() {
-    const token = Cookies.get('accessToken')
+    const token = Cookies.get('accessToken');
 
     if (token) {
-      isLoading.value = true
+      isLoading.value = true;
       try {
         const response = await axios.get(
           `${import.meta.env.VUE_APP_API_URL}/auth/user`,
@@ -21,30 +45,30 @@ export const useAuthStore = defineStore('auth', () => {
             headers: {
               Authorization: `Bearer ${token}`,
             },
-          },
-        )
+          }
+        );
 
         if (response.data) {
-          user.value = response.data.data.user
+          user.value = response.data.data.user as User;
         }
       } catch (error) {
-        console.error('Failed to fetch user data', error)
-        user.value = null
+        console.error('Failed to fetch user data', error);
+        user.value = null;
       } finally {
-        isLoading.value = false
+        isLoading.value = false;
       }
     }
   }
 
-  function setUser(userData) {
-    user.value = userData
+  function setUser(userData: User) {
+    user.value = userData;
   }
 
   watchEffect(() => {
     if (accessToken.value) {
-      fetchUserData()
+      fetchUserData();
     }
-  })
+  });
 
-  return { user, isLoading, isLoggedIn, fetchUserData, setUser }
-})
+  return { user, isLoading, isLoggedIn, fetchUserData, setUser };
+});
